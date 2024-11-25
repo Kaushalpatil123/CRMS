05a3aed2a4037a143b631d17761553b871e2ba25
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2024-11-25 17:42:37 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 app.use(
   cors({
-    origin: [/^http:\/\/localhost:\d+$/, "https://crms-frontend-sigma.vercel.app","http://localhost:3000"],
+    origin: [/^http:\/\/localhost:\d+$/, "https://crms-frontend-sigma.vercel.app","http://localhost:3000","https://crms-peach.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
