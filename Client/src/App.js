26509aa023f909549f7e190fbad6d3eb22b08ad0
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2025-01-15 14:44:47 +0530

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -41,19 +41,6 @@ const App = () => {
           <ScrollTop>
             <AuthProvider>
               <>
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/67877b2c825083258e05591f/1ihkji9t1';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
                 <Notistack>
                   <Routes />
                   {/* Dark mode  */}
